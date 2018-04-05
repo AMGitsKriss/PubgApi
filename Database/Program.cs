7b5d34551e9c1d5786bba8460aa644c9243d11e4
AMGitsKriss <sp4m@k-jessop.co.uk>
2018-04-06 00:50:17 +0100

    Initial Database Connection

--- Database/Program.cs
+++ Database/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,23 @@ namespace Database
     {
         static void Main(string[] args)
         {
+            MySql.Data.MySqlClient.MySqlConnection conn;
+            string myConnectionString;
+
+            myConnectionString = ConfigurationManager.AppSettings.Get("ConnectionString");
+
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = myConnectionString;
+                conn.Open();
+                Console.WriteLine("Connection made.");
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                throw ex;
+            }
+            Console.ReadKey();
         }
     }
 }
