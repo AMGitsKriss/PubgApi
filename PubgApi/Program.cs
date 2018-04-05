c52ca7aa99cbe338ca9b0353c13711be0767166c
AMGitsKriss <sp4m@k-jessop.co.uk>
2018-04-05 23:22:06 +0100

    Add project files.

---
+++ PubgApi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PubgApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
